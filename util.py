bd262f56798dc7e432c173142b896439987d5f91
Johannes Löthberg <johannes@kyriasis.com>
2016-06-18 21:49:24 +0200

    Move arithmetic functions to arithmetic.py
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- util.py
+++ util.py
@@ -48,36 +48,3 @@ def sequentialize(xs):
                 i += 1
 
     return res
-
-
-def mod_add(x, y):
-    return (x + y) % 10
-
-
-def mod_adds(xs, ys):
-    return [ mod_add(x, y) for x, y in zip(xs, ys)]
-
-
-def wrap_sub(x, y):
-    return (10 + (x - y)) % 10
-
-
-def wrap_subs(xs, ys):
-    return [ wrap_sub(x, y) for x, y in zip(xs, ys) ]
-
-
-def chain_add(xs, expand_by, done=[]):
-    xs = xs[::1]
-    if expand_by == 0:
-        return done + xs
-
-    x, xs = lpop(xs)
-    xs.append(mod_add(x, xs[0]))
-    done.append(x)
-
-    return chain_add(xs, expand_by - 1, done)
-
-
-def pa(a):
-    for row in a:
-        print('\t'.join([str(x) for x in row]))
